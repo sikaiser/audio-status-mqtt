5f85b6a151602edb11dc6796cb52ca316b8b611a
Simon <simon@emperor.ch>
2024-07-10 21:35:04 +0200

    send "paused" after 20 seconds, user counter var instead of buffer

--- audio_status_mqtt.sh
+++ audio_status_mqtt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 #This scripts sets the status to "playing" as soon as there is sound.
-#But for setting the status to "stopped" it waits a $BUFFER_SIZE amount of seconds.
+#After DELAY_PAUSED seconds of no sound, it sets the status to "paused".
+#And after DELAY_STOPPED seconds of no sound, it sets the status to "stopped".
 #When the status is "playing", the status is continuously published to MQTT.
-#When the last status was "stopped", no updates are sent until the status changes.
+#During the DELAY_PAUSED period, the status "playing" is not updated.
+#The statuses "paused" and "stopped" are sent only once.
 
 BROKER_IP="YOUR_MQTT_BROKER_IP"
 USERNAME="YOUR_MQTT_USERNAME"
@@ -13,10 +15,10 @@ DEVICE_NAME="YOUR_DEVICE_NAME"
 STATUS_TOPIC="${DEVICE_NAME}/audio/status"
 VOLUME_TOPIC="${DEVICE_NAME}/audio/volume"
 
-BUFFER_SIZE=20 # how many loops of continuous silence until "stopped" is called
+DELAY_PAUSED=20 # how many loops of continuous silence until "paused" is sent
+DELAY_PAUSED=120 # how many loops of continuous silence until "stopped" is sent
 
-previous_status=""
-volume_buffer=()
+count_no_volume=0
 
 continuous=true
 
@@ -27,32 +29,29 @@ while $continuous; do
     #returns both positive and negative values when audio is playing (the closer to 0, the lower the volume)
     volume=$(parec --raw --channels=1 --latency=2 2>/dev/null | od -N2 -td2 | head -n1 | cut -d' ' -f2- | tr -d ' ')
 
-    # Add absolute volume to buffer
-    volume_buffer+=(${volume#-})
-    if [ ${#volume_buffer[@]} -gt $BUFFER_SIZE ]; then
-        volume_buffer=("${volume_buffer[@]:1}")  # Remove the oldest value
-    fi
-
-    # Calculate sum of  volumes in buffer
-    sum_volume=0
-    for v in "${volume_buffer[@]}"; do
-        sum_volume=$((sum_volume + v))
-    done
 
     if (($volume != 0)); then
-        current_status="playing"
+		count_no_volume=0
     else
-        if (($sum_volume == 0)); then
-            current_status="stopped"
-        fi
+        count_no_volume=$((count_no_volume+1))
     fi
 
-	# Send update to MQTT while sound is playing or when status has changed.
-    if [[ $volume != 0 || "$current_status" != "$previous_status" ]]; then
-        mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $STATUS_TOPIC -m $current_status
-        mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $VOLUME_TOPIC -m $volume
-        previous_status=$current_status
-    fi
+    case $count_no_volume in	
+	    0)
+            mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $STATUS_TOPIC -m "playing"
+            mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $VOLUME_TOPIC -m $volume
+			;;
+		
+		$DELAY_PAUSED)
+            mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $STATUS_TOPIC -m "paused"
+            mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $VOLUME_TOPIC -m $volume
+			;;
+			
+		$DELAY_STOPPED)
+            mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $STATUS_TOPIC -m "stopped"
+            mosquitto_pub -h $BROKER_IP -u $USERNAME -P $PASSWORD -t $VOLUME_TOPIC -m $volume
+			;;		
+	esac
 
     if [ $debug = true ]; then
         continuous=false
